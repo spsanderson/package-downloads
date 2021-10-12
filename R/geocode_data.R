8ab6c382f33efec941dcbc24ce4575ebb8b52a12
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-10-12 10:24:56 -0400

    Update geocode_data.R

---
+++ R/geocode_data.R
@@ -0,0 +1,101 @@
+#' Mapping Data
+#'
+#' @return
+#' A tibble
+#'
+#' @export
+mapping_dataset <- function(){
+  if(file.exists("mapping_dataset.rds")){
+    map_data <- readRDS("mapping_dataset.rds") %>%
+      dplyr::as_tibble()
+  } else {
+    message("There is no mapping data present. Will attempt to create.")
+
+    if(!file.exists("old_downloads.RDS")){
+      message("There is nothing to geocode.")
+    } else {
+      df_tbl <- readRDS("old_downloads.rds") %>%
+        dplyr::as_tibble()
+
+      # Get countries as a character vector
+      country_vector <- df_tbl %>%
+        dplyr::mutate(
+          country_name = countrycode::countrycode(country, "iso2c", "country.name")
+        ) %>%
+        dplyr::select(country_name) %>%
+        dplyr::distinct() %>%
+        dplyr::filter(!is.na(country_name)) %>%
+        dplyr::pull()
+
+      # Map the geocode_OSM function to the vector
+      geocode_tbl <- country_vector %>%
+        purrr::map(
+          function(x) tmaptools::geocode_OSM(
+            x, return.first.only = TRUE, as.data.frame = TRUE, details = TRUE
+          )
+        ) %>%
+        purrr::map_dfr(~ base::as.data.frame(.))
+
+      # Coerce to a tibble and rename columns
+      geocode_map_tbl <- geocode_tbl %>%
+        dplyr::as_tibble() %>%
+        dplyr::select(query, lat, lon, display_name, icon) %>%
+        purrr::set_names("country","latitude","longitude","display_name","icon")
+
+      # Write file to RDS for later use in mapping
+      saveRDS(
+        object = geocode_map_tbl
+        , file = "mapping_dataset.rds"
+      )
+
+      map_data <- readRDS("mapping_dataset.rds")
+
+    }
+  }
+
+  return(map_data)
+}
+
+#' Leaflet Map
+#'
+#' @return
+#' A leaflet map
+#'
+#' @export
+map_leaflet <- function(){
+
+  df_tbl <- readRDS("old_downloads.rds")
+
+  country_count_tbl <- df_tbl %>%
+    dplyr::mutate(
+      country_name = countrycode::countrycode(country, "iso2c", "country.name")
+    ) %>%
+    dplyr::filter(!is.na(country_name)) %>%
+    dplyr::count(country_name)
+
+  geocode_map_tbl <- mapping_dataset()
+
+  # Map ---------------------------------------------------------------------
+  map_data <- geocode_map_tbl %>%
+    dplyr::left_join(country_count_tbl, by = c("country" = "country_name"))
+
+  l <- leaflet::leaflet(data = map_data) %>%
+    leaflet::addTiles() %>%
+    leaflet::addMarkers(
+      lng = ~longitude
+      , lat = ~latitude
+      , label = ~htmltools::htmlEscape(country)
+      , popup = ~as.character(
+          paste(
+            "<strong>Country: </strong>"
+            , country
+            , "<br><strong>Display Name: </strong>"
+            , display_name
+            , "<br><strong>Downloads: </strong>"
+            , n
+          )
+        )
+      )
+
+  return(l)
+}
