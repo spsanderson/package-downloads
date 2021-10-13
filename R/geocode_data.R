1697a849f857c382810a50bf23c5930cc1e1963a
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-10-13 10:46:18 -0400

    Update geocode_data.R

--- R/geocode_data.R
+++ R/geocode_data.R
@@ -10,6 +10,48 @@ mapping_dataset <- function(){
       dplyr::as_tibble()
 
     # make sure all countries are geocoded, if not then geocode them
+    current_country_vector <- map_data %>%
+      dplyr::pull(country)
+
+    tmp                    <- readRDS("old_downloads.RDS")
+    new_country_vector     <- tmp %>%
+      dplyr::mutate(
+        country_name = countrycode::countrycode(country, "iso2c", "country.name")
+      ) %>%
+      dplyr::select(country_name) %>%
+      dplyr::distinct() %>%
+      dplyr::filter(!is.na(country_name)) %>%
+      dplyr::pull()
+
+    countries_to_geocode <- setdiff(new_country_vector, current_country_vector)
+
+    if(length(countries_to_geocode) != 0){
+      new_geocode_tbl <- country_vector %>%
+        purrr::map(
+          function(x) tmaptools::geocode_OSM(
+            x, return.first.only = TRUE, as.data.frame = TRUE, details = TRUE
+          )
+        ) %>%
+        purrr::map_dfr(~ base::as.data.frame(.))
+
+      # Coerce to a tibble and rename columns
+      new_geocode_map_tbl <- geocode_tbl %>%
+        dplyr::as_tibble() %>%
+        dplyr::select(query, lat, lon, display_name, icon) %>%
+        purrr::set_names("country","latitude","longitude","display_name","icon")
+
+      map_data <- rbind(map_data, new_geocode_map_tbl)
+
+      # Write file to RDS for later use in mapping
+      saveRDS(
+        object = map_data
+        , file = "mapping_dataset.rds"
+      )
+
+      map_data <- readRDS("mapping_dataset.rds")
+
+    }
+
   } else {
     message("There is no mapping data present. Will attempt to create.")
 
