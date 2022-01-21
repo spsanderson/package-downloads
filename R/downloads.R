b031dac516c90860831a603f9e5506df928a1ab1
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-21 08:08:46 -0500

    Update downloads.R

--- R/downloads.R
+++ R/downloads.R
@@ -41,7 +41,8 @@ download_logs <- function(from = Sys.Date() - 9,
       file.remove(file)
       # Get only the dates and packages we want
       downloads[, date := as.Date(date)]
-      downloads[package %in% c("healthyR","healthyR.ts","healthyR.data","healthyverse","healthyR.ai")]
+      downloads[package %in% c("healthyR","healthyR.ts","healthyR.data",
+                               "healthyverse","healthyR.ai","TidyDensity")]
     })
     parallel::stopCluster(cl)
 
