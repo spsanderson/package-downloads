155e7546ed7cbf475a328c4603d7255c47663294
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-03-13 10:32:54 -0400

    change base theme font size

--- R/plot.R
+++ R/plot.R
@@ -38,7 +38,7 @@ plot_cumulative_downloads <- function(daily_downloads, color = "steelblue") {
     ) +
     scale_x_date(date_labels = "%b %d") +
     scale_y_continuous(expand = expansion(c(.05, .2))) +
-    theme_classic(base_size = 14) +
+    theme_classic(base_size = 11) +
     labs(
       x = NULL,
       y = NULL,
@@ -90,7 +90,7 @@ plot_cumulative_downloads_pkg <- function(.data, color = "steelblue"
     ) +
     scale_x_date(date_labels = "%b %d") +
     scale_y_continuous(expand = expansion(c(.05, .2))) +
-    theme_classic(base_size = 14) +
+    theme_classic(base_size = 11) +
     labs(
       x = NULL,
       y = NULL,
@@ -102,7 +102,7 @@ plot_cumulative_downloads_pkg <- function(.data, color = "steelblue"
 hist_daily_downloads <- function(daily_downloads, color = "steelblue") {
   ggplot(daily_downloads, aes(N)) +
     geom_histogram(bins = 7, fill = color, color = "black") +
-    theme_classic(base_size = 14) +
+    theme_classic(base_size = 11) +
     labs(
       x = NULL,
       y = NULL,
@@ -125,6 +125,6 @@ plot_downloads_by_country <- function(downloads_by_country, color = "steelblue")
       title = "Countries with Highest Total\nNumber of Downloads"
     ) +
     scale_y_continuous(expand = expansion(c(0, .15))) +
-    theme_classic(base_size = 14) +
+    theme_classic(base_size = 11) +
     ggeasy::easy_remove_x_axis()
 }
