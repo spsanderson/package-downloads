768a90ec92a955bd132a41c04efbd693b4b1a6e3
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-06-14 11:45:49 -0400

    Create data_refresh.sh

---
+++ bash/data_refresh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Rendering the page..."
+
+Rscript -e "rmarkdown::render(input = 'README.Rmd')"
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    git config --global user.name 'spsanderson'
+    git config --global user.email 'spsanderson@gmail.com'
+    git add *
+    git commit -m "Auto update Report"
+    git push
+fi
