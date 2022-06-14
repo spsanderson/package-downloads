37adfe8ecbb009b14b76a9beda0428739539c765
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-06-14 12:12:37 -0400

    Update data_refresh.sh

--- bash/data_refresh.sh
+++ bash/data_refresh.sh
@@ -8,6 +8,6 @@ if [[ "$(git status --porcelain)" != "" ]]; then
     git config --global user.name 'spsanderson'
     git config --global user.email 'spsanderson@gmail.com'
     git add *
-    git commit -m "Auto update Report"
+    git commit -m "Auto update dashboard"
     git push
 fi
